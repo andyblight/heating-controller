fc0b3f6017f08e553ed7b033bdeab055dff6feb1
Andy Blight <andy.blight@gmail.com>
2020-07-08 08:08:52 +0100

    Added location and timed loop

--- record-temps.py
+++ record-temps.py
@@ -1,20 +1,27 @@
 import requests
+import sched
+import time
+
+# pip3 imports
 from bs4 import BeautifulSoup
 
+LOCATIONS = [["http://192.168.2.63", "Upstairs"], ["http://192.168.2.64", "Downstairs"]]
+INTERVAL_TIME_S = 5
 
-URLS = ['http://192.168.2.63', 'http://192.168.2.64']
+# Set up scheduler
+scheduler = sched.scheduler(time.time, time.sleep)
 
 
 def extract_readings(page):
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
     # Temperature
-    temperature_item = soup.find(id='temperature')
+    temperature_item = soup.find(id="temperature")
     temperature_pretty = temperature_item.prettify()
     temperature_parts = temperature_pretty.split("\n")
     temperature = float(temperature_parts[1])
     # print("temperature", temperature)
     # Humidity
-    humidity_item = soup.find(id='humidity')
+    humidity_item = soup.find(id="humidity")
     humidity_pretty = humidity_item.prettify()
     humidity_parts = humidity_pretty.split("\n")
     humidity = float(humidity_parts[1])
@@ -22,11 +29,28 @@ def extract_readings(page):
     return (temperature, humidity)
 
 
-for url in URLS:
-    try:
-        response = requests.get(url)
-        readings = extract_readings(response)
-        print("URL", url, "readings", readings)
-    except requests.exceptions.RequestException as e:
-        _ = e  # sprint(e)
-        print("URL", url, "no response")
+def get_pages():
+    for url, location in LOCATIONS:
+        try:
+            response = requests.get(url)
+            readings = extract_readings(response)
+            print("{0:10s}: {1:4}C, {2:4}%".format(location, readings[0], readings[1]))
+        except requests.exceptions.RequestException as e:
+            _ = e  # sprint(e)
+            print(location, "no response")
+    # Queue next action
+    scheduler.enter(INTERVAL_TIME_S, 1, get_pages)
+
+
+def main():
+    get_pages()
+    # Run until all actions complete.
+    scheduler.run()
+
+
+if __name__ == "__main__":
+    main()
+
+# Add location to each URL. DONE.
+# Get pages every 1 minute. DONE.
+# Save readings to file with location and time stamp.
