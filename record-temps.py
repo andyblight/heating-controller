a23d3c8dcb5a77cf637ded2d7e078de94da5ca9b
Andy Blight <andy.blight@gmail.com>
2020-07-11 15:58:57 +0100

    Refactored to write file per location

--- record-temps.py
+++ record-temps.py
@@ -7,58 +7,91 @@ import time
 # pip3 imports
 from bs4 import BeautifulSoup
 
-LOCATIONS = [["http://192.168.2.63", "Upstairs"], ["http://192.168.2.64", "Downstairs"]]
+LOCATIONS = [["Upstairs", "http://192.168.2.63"], ["Downstairs", "http://192.168.2.64"]]
 INTERVAL_TIME_S = 5
+# INTERVAL_TIME_S = 60
 
 # Set up scheduler
 scheduler = sched.scheduler(time.time, time.sleep)
 
 
-def extract_readings(page):
-    soup = BeautifulSoup(page.content, "html.parser")
-    # Temperature
-    temperature_item = soup.find(id="temperature")
-    temperature_pretty = temperature_item.prettify()
-    temperature_parts = temperature_pretty.split("\n")
-    temperature = float(temperature_parts[1])
-    # print("temperature", temperature)
-    # Humidity
-    humidity_item = soup.find(id="humidity")
-    humidity_pretty = humidity_item.prettify()
-    humidity_parts = humidity_pretty.split("\n")
-    humidity = float(humidity_parts[1])
-    # print("humidity", humidity)
-    return (temperature, humidity)
+class TemperatureReader:
+    def __init__(self, location, url):
+        self.location = location
+        self.url = url
+        self.temperature = 0
+        self.humidity = 0
+        self.csv_file = None
+        self.csv_writer = None
 
+    def open_file(self):
+        file_name = self.location + ".csv"
+        self.csv_file = open(file_name, "w", newline="")
+        self.csv_writer = csv.writer(
+            self.csv_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        self.csv_writer.writerow(["Timestamp", "Location", "Temperature", "Humidity"])
+        self.csv_file.flush()
 
-def get_pages(csv_file, csv_writer):
-    for url, location in LOCATIONS:
+    def close_file(self):
+        self.csv_writer = None
+        self.csv_file.close()
+
+    def get_temps(self):
         try:
-            response = requests.get(url)
-            (temperature, humidity) = extract_readings(response)
-            time_now = datetime.now()
-            timestamp_seconds = int(time_now.timestamp())
-            print("{0:12d} {1:10s}: {2:4}C, {3:4}%".format(timestamp_seconds, location, temperature, humidity))
-            csv_writer.writerow([timestamp_seconds, location, temperature, humidity])
-            csv_file.flush()
+            page = requests.get(self.url)
+            self.extract_readings(page)
+            self.write_to_file()
         except requests.exceptions.RequestException as e:
             _ = e  # print(e)
             print(location, "no response")
+
+    def extract_readings(self, page):
+        soup = BeautifulSoup(page.content, "html.parser")
+        # Temperature
+        temperature_item = soup.find(id="temperature")
+        temperature_pretty = temperature_item.prettify()
+        temperature_parts = temperature_pretty.split("\n")
+        self.temperature = float(temperature_parts[1])
+        # print("temperature", temperature)
+        # Humidity
+        humidity_item = soup.find(id="humidity")
+        humidity_pretty = humidity_item.prettify()
+        humidity_parts = humidity_pretty.split("\n")
+        self.humidity = float(humidity_parts[1])
+        # print("humidity", humidity)
+
+    def write_to_file(self):
+        time_now = datetime.now()
+        timestamp_seconds = int(time_now.timestamp())
+        print(
+            "{0:12d} {1:10s}: {2:4}C, {3:4}%".format(
+                timestamp_seconds, self.location, self.temperature, self.humidity
+            )
+        )
+        self.csv_writer.writerow(
+            [timestamp_seconds, self.location, self.temperature, self.humidity]
+        )
+        self.csv_file.flush()
+
+
+def timed_read(readers):
+    for reader in readers:
+        reader.get_temps()
     # Queue next action.
-    scheduler.enter(INTERVAL_TIME_S, 1, get_pages, argument=(csv_file, csv_writer,))
+    scheduler.enter(INTERVAL_TIME_S, 1, timed_read, argument=(readers,))
 
 
 def main():
-    # Open CSV file.
-    with open("temperatures.csv", "w", newline="") as csv_file:
-        csv_writer = csv.writer(
-            csv_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
-        )
-        csv_writer.writerow(["Timestamp", "Location", "Temperature", "Humidity"])
-        # Get pages.
-        get_pages(csv_file, csv_writer)
-        # Run until all actions complete.
-        scheduler.run()
+    readers = []
+    for location, ip_address in LOCATIONS:
+        reader = TemperatureReader(location, ip_address)
+        reader.open_file()
+        readers.append(reader)
+    # Get pages.
+    timed_read(readers)
+    # Run until all actions complete.
+    scheduler.run()
 
 
 if __name__ == "__main__":
@@ -67,3 +100,9 @@ if __name__ == "__main__":
 # Add location to each URL. DONE.
 # Get pages every 1 minute. DONE.
 # Save readings to file with location and time stamp. DONE.
+# Create daily file for each location.
+# Change timestamp to human readable times.
+
+# Entities
+# Files
+# Locations
