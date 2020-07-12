be67a9e90d4d0b5c75182cb5a45c81c890c90c11
Andy Blight <andy.blight@gmail.com>
2020-07-12 21:59:10 +0100

    Writes to file

--- current-weather.py
+++ current-weather.py
@@ -2,6 +2,8 @@
 # Based on:
 # https://stackoverflow.com/questions/61358322/web-scrapping-specific-data-from-openweather-one-call-api-into-csv-file
 
+import csv
+from datetime import datetime, date
 import requests
 import sched
 import time
@@ -12,7 +14,7 @@ import pandas
 
 
 INTERVAL_TIME_S = 5
-# INTERVAL_TIME_S = 60
+# INTERVAL_TIME_S = 60 * 5
 
 # API call.
 # Tested this using a browser.
@@ -23,34 +25,92 @@ DEGREES_KELVIN = 273.15
 # Set up scheduler
 scheduler = sched.scheduler(time.time, time.sleep)
 
+f_csv_file = None
+f_csv_writer = None
+f_today = None
+
+
+def open_file():
+    print("of")
+    global f_today, f_csv_file, f_csv_writer
+    f_today = date.today()
+    file_name = f_today.isoformat() + "-external" + ".csv"
+    try:
+        f_csv_file = open(file_name, "w", newline="")
+        f_csv_writer = csv.writer(
+            f_csv_file, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
+        f_csv_writer.writerow(
+            ["Time", "Temperature C", "Humidity %", "Wind m/s", "Gust m/s"]
+        )
+    except OSError as err:
+        print("OS error: {0}".format(err))
+    print("f_today", f_today)
+
+def close_file():
+    print("clf")
+    global f_csv_file, f_csv_writer
+    f_csv_writer = None
+    if f_csv_file:
+        f_csv_file.close()
+
+
+def write_to_file(temperature, humidity, wind, degrees, gust):
+    print("wtf")
+    global f_csv_file, f_csv_writer
+    time_now = datetime.now()
+    time_str = time_now.strftime("%H:%M")
+    f_csv_writer.writerow([time_str, temperature_str, humidity, wind, degrees, gust])
+    f_csv_file.flush()
+
+
+def change_file():
+    print("chf")
+    global f_today
+    today = date.today()
+    print("f_today", f_today, "today", today)
+    if f_today != today:
+        close_file()
+        open_file()
+
 
 def get_current_weather():
     json_data = requests.get(OPEN_WEATHER_URL).json()
     print(json_data)
-    df = pandas.DataFrame(json_data['main'],index=[0])
-    print(df.to_string())
+    df = pandas.DataFrame(json_data["main"], index=[0])
+    # print(df.to_string())
     #     temp  feels_like  temp_min  temp_max  pressure  humidity
     # 0  293.4      292.37    291.48    295.37      1025        46
-    temp = df['temp'][0] - DEGREES_KELVIN
-    humidity = df['humidity'][0]
-    print("temp {:.1f}C, humidity {:d}%".format(temp, humidity))
-    df = pandas.DataFrame(json_data['wind'],index=[0])
-    print(df.to_string())
+    temperature = df["temp"][0] - DEGREES_KELVIN
+    humidity = df["humidity"][0]
+    print("temperature {:.1f}C, humidity {:d}%".format(temperature, humidity))
+    df = pandas.DataFrame(json_data["wind"], index=[0])
+    # print(df.to_string())
     #    speed  deg  gust
     # 0   0.89  266  2.68
-    wind_speed = df['speed'][0]
-    wind_degrees = df['deg'][0]
-    wind_gust = df['gust'][0]
-    print("wind speed {:.2f}, degrees {:d}, gust {:.2f}".format(wind_speed, wind_degrees, wind_gust))
+    wind_speed = df["speed"][0]
+    wind_degrees = df["deg"][0]
+    try:
+        wind_gust = df["gust"][0]
+    except KeyError:
+        wind_gust = 0.0
+    print(
+        "wind speed {:.2f}, degrees {:d}, gust {:.2f}".format(
+            wind_speed, wind_degrees, wind_gust
+        )
+    )
+    write_to_file(temperature, humidity, wind_speed, wind_degrees, wind_gust)
 
 
 def timed_read():
+    change_file()
     get_current_weather()
     # Queue next action.
     scheduler.enter(INTERVAL_TIME_S, 1, timed_read)
 
 
 def main():
+    open_file()
     # Get pages.
     timed_read()
     # Run until all actions complete.
