8d40667f891d73248de8e7f8c26564ae7aecc818
Andy Blight <andy.blight@gmail.com>
2020-07-12 19:23:32 +0100

    Extracted data out of response

--- current-weather.py
+++ current-weather.py
@@ -18,6 +18,8 @@ INTERVAL_TIME_S = 5
 # Tested this using a browser.
 OPEN_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?id=2640132&appid=96f114b6e51c0d7e6de6f4f19dd522ee"
 
+DEGREES_KELVIN = 273.15
+
 # Set up scheduler
 scheduler = sched.scheduler(time.time, time.sleep)
 
@@ -27,8 +29,19 @@ def get_current_weather():
     print(json_data)
     df = pandas.DataFrame(json_data['main'],index=[0])
     print(df.to_string())
+    #     temp  feels_like  temp_min  temp_max  pressure  humidity
+    # 0  293.4      292.37    291.48    295.37      1025        46
+    temp = df['temp'][0] - DEGREES_KELVIN
+    humidity = df['humidity'][0]
+    print("temp {:.1f}C, humidity {:d}%".format(temp, humidity))
     df = pandas.DataFrame(json_data['wind'],index=[0])
     print(df.to_string())
+    #    speed  deg  gust
+    # 0   0.89  266  2.68
+    wind_speed = df['speed'][0]
+    wind_degrees = df['deg'][0]
+    wind_gust = df['gust'][0]
+    print("wind speed {:.2f}, degrees {:d}, gust {:.2f}".format(wind_speed, wind_degrees, wind_gust))
 
 
 def timed_read():
