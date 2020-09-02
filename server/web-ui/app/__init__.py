08190eafd5cf7ccee256bc8a7398b36ddcfabfab
Andy Blight <andy.blight@gmail.com>
2020-09-02 20:57:20 +0100

    Web-UI: moved chart functions to __init__.py

--- server/web-ui/app/__init__.py
+++ server/web-ui/app/__init__.py
@@ -1,9 +1,73 @@
 from flask import Flask
 from config import Config
+import gviz_api
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+def chart_today():
+    description = {'day': ('number', 'Day'),
+                   'guardians': ('number', 'Guardians of the Galaxy'),
+                   'avengers': ('number', 'The Avengers'),
+                   'transformers': ('number', 'Transformers: Age of Extinction')}
+    data = [
+        {'day':  1, 'guardians': 37.8, 'avengers': 80.8, 'transformers': 41.8},
+        {'day':  2, 'guardians': 30.9, 'avengers': 69.5, 'transformers': 32.4},
+        {'day':  3, 'guardians': 25.4, 'avengers': 57.0, 'transformers': 25.7},
+        {'day':  4, 'guardians': 11.7, 'avengers': 18.8, 'transformers': 10.5},
+    ]
+    chart_today = gviz_api.DataTable(description)
+    chart_today.LoadData(data)
+    jscode_today = chart_today.ToJSCode('jscode_chart_today',
+                                        columns_order=('day',
+                                                'guardians',
+                                                 'avengers',
+                                                 'transformers'))
+    return jscode_today
+
+
+def chart_seven_days():
+    description = {'day': ('number', 'Day'),
+                   'guardians': ('number', 'Guardians of the Galaxy'),
+                   'avengers': ('number', 'The Avengers'),
+                   'transformers': ('number', 'Transformers: Age of Extinction')}
+    data = [
+        {'day':  1, 'guardians': 37.8, 'avengers': 80.8, 'transformers': 41.8},
+        {'day':  2, 'guardians': 30.9, 'avengers': 69.5, 'transformers': 32.4},
+        {'day':  3, 'guardians': 25.4, 'avengers': 57.0, 'transformers': 25.7},
+        {'day':  4, 'guardians': 11.7, 'avengers': 18.8, 'transformers': 10.5},
+    ]
+    chart_seven = gviz_api.DataTable(description)
+    chart_seven.LoadData(data)
+    jscode_seven = chart_seven.ToJSCode('jscode_chart_seven',
+                                       columns_order=('day',
+                                                'guardians',
+                                                 'avengers',
+                                                 'transformers'))
+    return jscode_seven
+
+
+def chart_thirty_days():
+    description = {'day': ('number', 'Day'),
+                   'guardians': ('number', 'Guardians of the Galaxy'),
+                   'avengers': ('number', 'The Avengers'),
+                   'transformers': ('number', 'Transformers: Age of Extinction')}
+    data = [
+        {'day':  1, 'guardians': 37.8, 'avengers': 80.8, 'transformers': 41.8},
+        {'day':  2, 'guardians': 30.9, 'avengers': 69.5, 'transformers': 32.4},
+        {'day':  3, 'guardians': 25.4, 'avengers': 57.0, 'transformers': 25.7},
+        {'day':  4, 'guardians': 11.7, 'avengers': 18.8, 'transformers': 10.5},
+    ]
+    chart_thirty = gviz_api.DataTable(description)
+    chart_thirty.LoadData(data)
+    jscode_thirty = chart_thirty.ToJSCode('jscode_chart_thirty',
+                                        columns_order=('day',
+                                                'guardians',
+                                                 'avengers',
+                                                 'transformers'))
+    return jscode_thirty
 
 
 from app import routes  # noqa: 401
--- server/web-ui/app/routes.py
+++ server/web-ui/app/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template
 from flask_table import Table, Col
-import gviz_api
-from app import app
+from app import app, chart_today, chart_seven_days, chart_thirty_days
 
 
 class SensorTable(Table):
@@ -34,37 +33,9 @@ def index():
 
 @app.route('/charts')
 def charts():
-    description = {'day': ('number', 'Day'),
-                   'guardians': ('number', 'Guardians of the Galaxy'),
-                   'avengers': ('number', 'The Avengers'),
-                   'transformers': ('number', 'Transformers: Age of Extinction')}
-    data = [
-        {'day':  1, 'guardians': 37.8, 'avengers': 80.8, 'transformers': 41.8},
-        {'day':  2, 'guardians': 30.9, 'avengers': 69.5, 'transformers': 32.4},
-        {'day':  3, 'guardians': 25.4, 'avengers': 57.0, 'transformers': 25.7},
-        {'day':  4, 'guardians': 11.7, 'avengers': 18.8, 'transformers': 10.5},
-    ]
-    chart_today = gviz_api.DataTable(description)
-    chart_today.LoadData(data)
-    jscode_today = chart_today.ToJSCode('jscode_chart_today',
-                                        columns_order=('day',
-                                                'guardians',
-                                                 'avengers',
-                                                 'transformers'))
-    chart_seven = gviz_api.DataTable(description)
-    chart_seven.LoadData(data)
-    jscode_seven = chart_seven.ToJSCode('jscode_chart_seven',
-                                       columns_order=('day',
-                                                'guardians',
-                                                 'avengers',
-                                                 'transformers'))
-    chart_thirty = gviz_api.DataTable(description)
-    chart_thirty.LoadData(data)
-    jscode_thirty = chart_thirty.ToJSCode('jscode_chart_thirty',
-                                        columns_order=('day',
-                                                'guardians',
-                                                 'avengers',
-                                                 'transformers'))
+    jscode_today = chart_today()
+    jscode_seven = chart_seven_days()
+    jscode_thirty = chart_thirty_days()
     return render_template(
         'charts.html',
         title='Charts', jscode_chart_today=jscode_today, jscode_chart_seven=jscode_seven, jscode_chart_thirty=jscode_thirty)
