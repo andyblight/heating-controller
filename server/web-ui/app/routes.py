c3abd7029b3f67e2410f94b233b63a8d33ebf271
Andy Blight <andy.blight@gmail.com>
2020-09-02 21:00:47 +0100

    Web-UI: moved chart functions to new file, charts.py

--- server/web-ui/app/routes.py
+++ server/web-ui/app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask_table import Table, Col
-from app import app, chart_today, chart_seven_days, chart_thirty_days
+from app import app
+from app.charts import chart_today, chart_seven_days, chart_thirty_days
 
 
 class SensorTable(Table):
